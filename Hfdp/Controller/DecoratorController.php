e74d829ba1be7071e6577aac7716c81b18874eb3
andrey <andrewenka@gmail.com>
2017-08-09 01:53:54 +0300

    Decorator modified.

--- Hfdp/Controller/DecoratorController.php
+++ Hfdp/Controller/DecoratorController.php
@@ -12,19 +12,19 @@ class DecoratorController
     public function main()
     {
         $beverage = new Decorator\Espresso();
-        \Hfdp\Utils::outputWithBr($beverage->getDescription() . " $" . $beverage->cost());
+        echo $beverage->getDescription() . " $" . $beverage->cost() . "<br />";
         
         $beverage2 = new Decorator\DarkRoast();
         $beverage2 = new Decorator\Mocha($beverage2);
         $beverage2 = new Decorator\Mocha($beverage2);
         $beverage2 = new Decorator\Whip($beverage2);
-        \Hfdp\Utils::outputWithBr($beverage2->getDescription() . " $" . $beverage2->cost());
+        echo $beverage2->getDescription() . " $" . $beverage2->cost() . "<br />";
         
         $beverage3 = new Decorator\HouseBlend();
         $beverage3 = new Decorator\Soy($beverage3);
         $beverage3 = new Decorator\Mocha($beverage3);
         $beverage3 = new Decorator\Whip($beverage3);
-        \Hfdp\Utils::outputWithBr($beverage3->getDescription() . " $" . $beverage3->cost());
+        echo $beverage3->getDescription() . " $" . $beverage3->cost() . "<br />";
     }
 
 }
