2905feba3812911e09ebcde1bc5e9c25da85d631
andrey <andrewenka@gmail.com>
2017-10-25 13:48:46 +0300

    Format.

--- autoloader.php
+++ autoloader.php
@@ -1,14 +1,16 @@
 <?php
 
-class Autoloader {
-    static public function loader($className) {
+class Autoloader
+{
+    static public function loader($className)
+    {
         $filename = str_replace("\\", "/", $className) . ".php";
         
         if (file_exists($filename)) {
             require_once($filename);
             return true;
         }
-        
+
         return false;
     }
 }
